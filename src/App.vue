fab133c266f47bf1f6b8bfbe66a9548538aaefc0
vadim-dmitriev <dva3213@gmail.com>
2023-01-28 18:43:41 +0300

    add VoiceAssistnt Vue component

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,60 @@
 <template>
-	<div id="app">
-		<AnalogClock />
-		<Weather />
-	</div>
+  <div id="app">
+    <AnalogClock />
+    <Weather />
+    <VoiceAssistant />
+  </div>
 </template>
 
 <script>
-import AnalogClock from './components/AnalogClock.vue';
-import Weather from './components/Weather.vue';
+import Vue from "vue";
+
+import AnalogClock from "./components/AnalogClock.vue";
+import Weather from "./components/Weather.vue";
+import VoiceAssistant from "./components/VoiceAssistant.vue";
+
+export const EventBus = new Vue();
 
 export default {
-	name: 'App',
-	components: {
-		AnalogClock,
-		Weather,
-	}
-}
+  name: "App",
+  components: {
+    AnalogClock,
+    Weather,
+    VoiceAssistant,
+  },
+  data: function () {
+    return {
+      wsConnection: null,
+    };
+  },
+  created: function () {
+    this.wsConnection = new WebSocket("ws://localhost:8081/ws");
+
+    this.wsConnection.onopen = function () {
+      console.log("Successfully connected to WS Server");
+    };
+
+    this.wsConnection.onmessage = function (event) {
+      if (event.data == "Keyword") {
+        EventBus.$emit("VoiceAssistant", event.data);
+      }
+    };
+  },
+};
 </script>
 
 <style>
 @font-face {
-	font-family: "Roboto";
-	src: url("~@/assets/fonts/Roboto-Thin.ttf");
+  font-family: "Roboto";
+  src: url("~@/assets/fonts/Roboto-Thin.ttf");
 }
 
 html {
-	background-color: #000;
-	color: #eee;
-	font-family: 'Roboto', sans-serif;
+  background-color: #000;
+  color: #eee;
+  font-family: "Roboto", sans-serif;
 }
 
-#app {}
+#app {
+}
 </style>
---
+++ src/components/VoiceAssistant.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <img src="@/assets/gif/voice_assistant_2.gif" v-bind:hidden="hidden" />
+  </div>
+</template>
+
+<script>
+import { EventBus } from "@/App.vue";
+
+export default {
+  name: "VoiceAssistant",
+  data: function () {
+    return {
+      hidden: true,
+    };
+  },
+  created: function () {
+    EventBus.$on("VoiceAssistant", () => {
+      this.hidden = !this.hidden;
+    });
+  },
+};
+</script>
+
+<style>
+img {
+  animation-direction: alternate;
+  display: block;
+  opacity: 1;
+  transition: opacity 1s ease-out;
+  -moz-transition: opacity 1s ease-out;
+  -webkit-transition: opacity 1s ease-out;
+  -o-transition: opacity 1s ease-out;
+}
+
+img[hidden] {
+  animation-direction: alternate;
+  opacity: 0;
+  transition: opacity 1s ease-out;
+  -moz-transition: opacity 1s ease-out;
+  -webkit-transition: opacity 1s ease-out;
+  -o-transition: opacity 1s ease-out;
+}
+</style>
